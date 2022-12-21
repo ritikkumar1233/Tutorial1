0e965e126921de7415e318cdad7f3d8828411974
Ritik Roushan Kumar <119618542+ritikkumar1233@users.noreply.github.com>
2022-12-21 21:55:12 +0530

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,7 @@
+l = [54,44,27,79,91,41]
+element = l.pop(4)
+print("List After removing element at index 4 ", l)
+l.insert(2, element)
+print("List after Adding element at index 2 ", l)
+l.append(element)
+print("List after Adding element at last ", l)
